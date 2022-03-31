7c0b7952cf54e1d66b43465ae371c8cf203fd02a
Makchcjp650 <99318126+Makchcjp650@users.noreply.github.com>
2022-03-31 17:32:51 +0300

    lab5.6
    lab5.6

---
+++ lab.rob_5.6,3/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double x = 6.2;
+    double y = 3.6;
+    System.out.println(Math.round (x));
+    System.out.println(Math.round(y));
+    }
+}
